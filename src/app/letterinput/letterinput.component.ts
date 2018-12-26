c21e934b741b8a52c626e1b00fa36494304cda35
Peter Borkuti <peterborkuti@gmail.com>
2018-12-26 08:34:46 +0100

    design letterinput

--- src/app/letterinput/letterinput.component.ts
+++ src/app/letterinput/letterinput.component.ts
@@ -3,15 +3,23 @@ import { Store } from '@ngrx/store';
 import { WordFinderState } from '../../store/model';
 import { LettersInput } from '../../store/actions';
 
+import { FormBuilder, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-letterinput',
   templateUrl: './letterinput.component.html',
   styleUrls: ['./letterinput.component.less']
 })
 export class LetterinputComponent implements OnInit {
+  lettersForm = this.fb.group({
+    lettersInput: [null, Validators.compose([
+      Validators.required, Validators.minLength(9), Validators.maxLength(9)])
+    ]
+  });
+
   _letters = '';
 
-  constructor(private store: Store<WordFinderState>) {}
+  constructor(private store: Store<WordFinderState>, private fb: FormBuilder) {}
 
   @Input()
   public set letters(val: string) {
