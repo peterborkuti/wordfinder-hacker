06e95e23e621d6524f01388e8466730fed4f5e73
Peter Borkuti <peterborkuti@gmail.com>
2018-12-26 08:51:54 +0100

    design for wordlist

--- src/app/wordlist/wordlist.component.ts
+++ src/app/wordlist/wordlist.component.ts
@@ -5,15 +5,21 @@ import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { FormBuilder, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-wordlist',
   templateUrl: './wordlist.component.html',
   styleUrls: ['./wordlist.component.less']
 })
 export class WordlistComponent implements OnInit {
+  wordlistForm = this.fb.group({
+    wordList: [null, Validators.required],
+  });
+
   wordfinder$: Observable<WordFinderState>;
 
-  constructor(private store: Store<WordFinderState>) {
+  constructor(private store: Store<WordFinderState>, private fb: FormBuilder) {
     this.wordfinder$ = store.pipe(select('wordfinder'), map(o => o.words));
   }
 
