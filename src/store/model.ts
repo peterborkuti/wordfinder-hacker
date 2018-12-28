c286652a8d11709c1ad18ee0bbad589151c1b9ea
Peter Borkuti <peterborkuti@gmail.com>
2018-12-28 09:17:11 +0100

    selectors to model

--- src/store/model.ts
+++ src/store/model.ts
@@ -1,4 +1,9 @@
-export class WordFinderState {
-  constructor(readonly letters = '', readonly words = [], readonly wordIndexToShow = -1) {}
-}
+import { createSelector } from '@ngrx/store';
 
+export const selectWordfinder = (state: any) => state.wordfinder;
+export const selectWords = createSelector(selectWordfinder, (state: WordFinderState) => state.words);
+export const selectLetters = createSelector(selectWordfinder, (state: WordFinderState) => state.letters);
+
+export class WordFinderState {
+  constructor(readonly letters = '', readonly words: string[] = [], readonly wordIndexToShow = -1) {}
+}
